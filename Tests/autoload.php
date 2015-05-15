a4f20b6765c26203f041123d11fee9394e57d84d
Markus Tacker <m@coderbyheart.de>
2015-05-15 20:15:14 +0200

    Moved again.

--- Tests/autoload.php
+++ Tests/autoload.php
@@ -1,10 +1,12 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    if (strpos($class, 'Dothiv') === 0) {
+    if (strpos($class, 'Dothiv\Bundle\ContentfulBundle') === 0) {
         $parts = explode('\\', $class);
         array_shift($parts);
-        require_once __DIR__ . '/../Dothiv/' . join(DIRECTORY_SEPARATOR, $parts) . '.php';
+        array_shift($parts);
+        array_shift($parts);
+        require_once __DIR__ . '/../' . join(DIRECTORY_SEPARATOR, $parts) . '.php';
         return true;
     }
     return false;
